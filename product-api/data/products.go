b2ef4bc5708f0de0836b54bc732b23778322fa7c
Wesley Reisz <wesleyreisz@section.io>
2020-05-08 10:09:01 -0400

    files from session 5

--- product-api/data/products.go
+++ product-api/data/products.go
@@ -7,7 +7,7 @@ import (
 	"time"
 )
 
-//Product defines the structure of the API product
+// Product defines the structure for an API product
 type Product struct {
 	ID          int     `json:"id"`
 	Name        string  `json:"name"`
@@ -24,31 +24,36 @@ func (p *Product) FromJSON(r io.Reader) error {
 	return e.Decode(p)
 }
 
+// Products is a collection of Product
 type Products []*Product
 
+// ToJSON serializes the contents of the collection to JSON
+// NewEncoder provides better performance than json.Unmarshal as it does not
+// have to buffer the output into an in memory slice of bytes
+// this reduces allocations and the overheads of the service
+//
+// https://golang.org/pkg/encoding/json/#NewEncoder
 func (p *Products) ToJSON(w io.Writer) error {
 	e := json.NewEncoder(w)
 	return e.Encode(p)
+}
+
+// GetProducts returns a list of products
+func GetProducts() Products {
+	return productList
 }
 
 func AddProduct(p *Product) {
 	p.ID = getNextID()
 	productList = append(productList, p)
 }
-func getNextID() int {
-	lp := productList[len(productList)-1]
-	return lp.ID + 1
-}
 
-func GetProducts() Products {
-	return productList
-}
-
-func UpdateProduct(id int, p *Product) error {
+func UpdateProduct(id int, p*Product) error {
 	_, pos, err := findProduct(id)
 	if err != nil {
 		return err
 	}
+
 	p.ID = id
 	productList[pos] = p
 
@@ -63,16 +68,24 @@ func findProduct(id int) (*Product, int, error) {
 			return p, i, nil
 		}
 	}
-	return nil, 0, ErrProductNotFound
+
+	return nil, -1, ErrProductNotFound 
 }
 
+func getNextID() int {
+	lp := productList[len(productList)-1]
+	return lp.ID + 1
+}
+
+// productList is a hard coded list of products for this
+// example data source
 var productList = []*Product{
 	&Product{
 		ID:          1,
 		Name:        "Latte",
 		Description: "Frothy milky coffee",
 		Price:       2.45,
-		SKU:         "abc123",
+		SKU:         "abc323",
 		CreatedOn:   time.Now().UTC().String(),
 		UpdatedOn:   time.Now().UTC().String(),
 	},
